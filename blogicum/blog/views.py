da7feabf743671f456ff7f39ad518e17c7d85087
4udo <4udodeev@gmail.com>
2024-02-07 20:22:59 +0300

    Вторая итерация

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,6 +1,10 @@
+from django.http import Http404
 from django.shortcuts import render
 
-posts = [
+from typing import Dict, List, Union
+
+
+posts: List[Dict[str, Union[int, str]]] = [
     {
         'id': 0,
         'location': 'Остров отчаянья',
@@ -43,13 +47,21 @@ posts = [
     },
 ]
 
+posts_dict = {post['id']: post for post in posts}
+
 
 def index(request):
     return render(request, 'blog/index.html', {'posts': posts})
 
 
-def post_detail(request, id):
-    return render(request, 'blog/detail.html', {'post': posts[id]})
+def post_detail(request, post_id):
+    post = posts_dict.get(post_id)
+    if post is None:
+        raise Http404(f"Пост с id={post_id} не найден.")
+
+    post['text_lines'] = post['text'].split('\n')
+
+    return render(request, 'blog/detail.html', {'post': post})
 
 
 def category_posts(request, category_slug):
